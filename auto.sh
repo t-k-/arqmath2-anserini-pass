4541ad7d409ab929b64f59166ebefb80a0050487
t-k- <clock126@126.com>
2021-05-21 03:55:22 -0400

    correct path

--- auto.sh
+++ auto.sh
@@ -8,11 +8,11 @@ _2021_task2_topics_refined=/tuna1/scratch/w32zhong/a0-engine/pya0/topics-and-qre
 set -xe
 # ### convert corpus ###
 # python prepare_corpus_crys.py --corpus /tuna1/scratch/w32zhong/mnt-corpus-task1.img --output corpus.converted/task1
-# python prepare_corpus_crys.py --corpus /tuna1/scratch/w32zhong/mnt-corpus-task2-visual.img --output corpus.converted/task2-visual
+python prepare_corpus_crys.py --corpus /tuna1/scratch/w32zhong/mnt-corpus-task2.img --output corpus.converted/task2
 # 
 # ### index ###
 # ./genn-anserini-index.sh corpus.converted/task1/ indexes/task1/
-# ./genn-anserini-index.sh corpus.converted/task2-visual/ indexes/task2-visual/
+./genn-anserini-index.sh corpus.converted/task2/ indexes/task2/
 
 ### convert topics ###
 mkdir -p topics.converted
@@ -24,6 +24,5 @@ python prepare_topic_from_tsv.py -i $_2021_task2_topics -o topics.converted/$(ba
 python prepare_topic_from_tsv.py -i $_2021_task2_topics_refined -o topics.converted/$(basename $_2021_task2_topics_refined)
 
 ### search and generate runs ###
-ln -sf `pwd`/indexes/task2-visual indexes/task2
 ln -sf `pwd`/topics.converted/topics.arqmath-2020-task1.json topics.converted/topics.arqmath-2020-task1.txt
 ./genn-anserini-run.sh
